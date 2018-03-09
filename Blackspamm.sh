14024219dc7ba9cc6c68957426d784b9674c20a8
BLACheat78 <37225106+BLACheat78@users.noreply.github.com>
2018-03-10 03:05:08 +0700

    Add files via upload

---
+++ Blackspamm.sh
@@ -0,0 +1,101 @@
+#!/system/xbin/bash
+clear
+figlet -f mini "ALL Spamm BlacK Heat78" | lolcat
+sleep 1
+echo "\033[30;1m By. Blackheat78"
+sleep 1
+echo "pilil nomor sepamm yang anda mau" | lolcat
+echo "1.JD.ID    |  2.Tokopedia  |  3.Telkomsel  |  4.Matahari" | lolcat
+echo "5.HOOQ     |  6.KPC        |  7.PHD        |  8.Whiskas" | lolcat
+echo "9.ZiPAY    |  0.KELUAR" | lolcat
+echo "\033[30;1m Pilihan Nomor :"
+sleep 1
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[34;1m"
+figlet "JD.ID"
+php 111.php
+fi
+
+if [ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[34;1m"
+figlet "tokopedia"
+php 222.php
+fi
+
+if
+[ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Telkomsel"
+php 333.php
+fi
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+echo "\033[31;1m"
+figlet "Matahari"
+php 444.php
+fi
+
+if [ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+echo "\033[33;1m"
+toilet "H00Q"
+php 555.php
+fi
+
+if [ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f slant --gay "KFC"
+echo "\033[31;1m"
+php 666.php
+fi
+
+if [ $mrrm = 7 ] || [ $mrrm = 7 ]
+then
+clear
+toilet -f mono12 -F gay "PHD"
+echo "\033[30;1m"
+php 777.php
+fi
+
+if [ $mrrm = 8 ] || [ $mrrm = 8 ]
+then
+clear
+echo "\033[34;1m"
+toilet "whiskas"
+php 888.php
+fi
+
+if [ $mrrm = 9 ] || [ $mrrm = 9 ]
+then
+clear
+echo "\33[33;1m"
+figlet "ZiPAY"
+php 999.php
+fi
+
+if [ $mrrm = 99 ] || [ $mrrm = 99 ]
+then
+cd sc
+sh i
+cd ..
+sh spam
+fi
+
+if [ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+exit
+fi
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# spammblackheat78
-Semua spamm sms black heat78
+# LITESPAM
+Assalamualaikum :) sebelumnya terimakasih telah memakai tool ini walaupun hanya sekedar penggabungan :( Cara menggunakannya cukup mudah Tinggal clone Author: Mr.Rm Team: Indonesia Security Lite Code: sh
